4cd2a34a45537558222152c6f7a94b137a7e5f88
gabiviktoria <esguerra.gabrielle12@gmail.com>
2025-07-29 11:56:10 +1200

    Added the nav bar for the Quiz matching the theme of the question box for consistency yay

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Title from '../Components/Title';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Title />
         {children}
       </body>
     </html>

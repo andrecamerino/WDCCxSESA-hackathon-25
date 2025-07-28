ccfe53e5a85c9e2ca24c2ded2432a5e889cb092b
ashoomky <ashlee@shum.co.nz>
2025-07-28 21:52:51 +1200

    feat: fixed syntax

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,15 +6,17 @@ import bgImage from './lucas.jpg'; // if you're importing a local image
 const Page = () => {
   return (
     <div
-      <HeaderComponent />
+
       style={{
-        backgroundImage: `url('/assets/lucas.jpeg')`, 
+        backgroundImage: `url('/assets/lucas.jpeg')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         height: '100vh',
       }}
     >
+
       <h1 style={{ color: 'white' }}>Welcome</h1>
+      < HeaderComponent />
     </div>
   );
 };

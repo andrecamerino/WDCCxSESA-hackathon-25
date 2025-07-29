59e94aac7bca729da18ad62f48d14ab837915f72
andrecamerino <camerinoandre@gmail.com>
2025-07-29 16:43:53 +1200

    move to components folder

--- src/app/Components/MCQquestions.tsx
+++ src/app/Components/MCQquestions.tsx
@@ -1,4 +1,4 @@
-// src/components/MCQQuestion.tsx
+'use client';
 import React from "react";
 
 type Props = {
@@ -9,14 +9,16 @@ type Props = {
 };
 
 const MCQQuestion: React.FC<Props> = ({ question, choices, selected, onSelect }) => {
-  
   return (
-    <div className="p-4 border rounded-xl">
+    <div className="p-4 justify-center items-center">
       <h2 className="text-xl mb-4">{question}</h2>
-      <ul>
+      <ul className="grid grid-cols-2 gap-4">
         {choices.map((choice, index) => (
           <li key={index}>
             <button
+              className={`w-full p-2 rounded text-black ${
+                selected === index ? 'bg-gray-500' : 'bg-gray-300 hover:bg-gray-400'
+              }`}
               onClick={() => onSelect(index)}
             >
               {choice}

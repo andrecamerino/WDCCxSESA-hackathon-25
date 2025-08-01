d08cf12cecacd59f0a79854fa2e9f91756d8cf6d
Andre Camerino <rcam301@UoA.auckland.ac.nz>
2025-08-01 12:25:06 +1200

    empty homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,21 +7,8 @@ import Questions from '../Components/Questions';
 
 const Page = () => {
   return (
-    <div
-
-    className='flex flex-col items-center justify-center scroll-smooth'
-
-      style={{
-        backgroundImage: `url('/assets/lucas.jpeg')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '100vh',
-      }}
-    >
-
-      < HeaderComponent />
-
-      <Questions />
+    <div>
+      Page
     </div>
   );
 };

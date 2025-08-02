fb968c1e76bb60a2b2386e578cf1b9903340b0d7
Andre Camerino <camerinoandre@gmail.com>
2025-08-03 07:19:27 +1200

    fix header

--- src/app/Components/ParentHeader.tsx
+++ src/app/Components/ParentHeader.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { CgProfile } from "react-icons/cg";
-import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="w-full bg-transparent">
+    <div className="w-full bg-transparent fixed">
       <div className="grid grid-cols-[1fr_1fr_1fr] justify-evenly items-center w-full py-8 text-2xl font-medium text-black">
-        <a href="/ProfilePage" className="flex">
-        </a>
-        <a href="/Home" className="text-2xl">
+        <a href="/Parents/HomePage" className="text-2xl">
           PiggyQuest
         </a>
-        <a href="/ProfilePage" className="justify-center flex">
+        <a href="/Parents/ProfilePage" className="justify-center flex">
           <CgProfile size={40} />
         </a>
       </div>

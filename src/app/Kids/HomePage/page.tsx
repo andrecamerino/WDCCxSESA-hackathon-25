85e6e1030fb558f971aac7b86912a5d38662bf21
LucasaFontaine <78568019+LucasaFontaine@users.noreply.github.com>
2025-08-03 11:24:57 +1200

    clouds moving on kids homepage

--- src/app/Kids/HomePage/page.tsx
+++ src/app/Kids/HomePage/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-
 import NotificationContainer from "../../Components/NotificationContainer";
 import Image from "next/image";
 
@@ -14,15 +13,38 @@ const Page = () => {
         backgroundPosition: "center",
         minHeight: "100vh",
       }}
+      
       className="overflow-hidden pt-20"
     >
-      <Image
-        src="/assets/piggy.png"
-        alt="Piggy Quest"
-        width={150}
-        height={150}
-        className="mx-auto mt-10"
-      />
+      {/* Clouds Layer */}
+      <div className="relative w-full h-40 mt-10 overflow-hidden">
+        <Image
+          src="/assets/cloud.png"
+          alt="Cloud 1"
+          width={100}
+          height={60}
+          className="absolute top-4 animate-cloud-left-to-right opacity-100"
+        />
+
+        <Image
+          src="/assets/cloud.png"
+          alt="Cloud 2"
+          width={120}
+          height={70}
+          className="absolute top-20 animate-cloud-left-to-right-slow opacity-100"
+        />
+
+        {/* Piggy Image */}
+        <div className="relative z-10 flex justify-center">
+          <Image
+            src="/assets/piggy.png"
+            alt="Piggy Quest"
+            width={150}
+            height={150}
+            className="mx-auto"
+          />
+        </div>
+      </div>
       <NotificationContainer />
     </div>
   );

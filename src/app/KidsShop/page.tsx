1f0c7577bd7d4b604bf43241a05305c315297e01
Andre Camerino <camerinoandre@gmail.com>
2025-08-02 21:53:52 +1200

    put in container

--- src/app/KidsShop/page.tsx
+++ src/app/KidsShop/page.tsx
@@ -1,22 +1,14 @@
 'use client';
 
 import React from 'react';
-import KidItem from '../Components/KidItem';
 import '../globals.css'; // Ensure global styles are imported
+import ShopItemContainer from '../Components/ShopItemContainer';
 
 const KidsShopPage: React.FC = () => {
     return (
-        <>
-            <div
-                className="border-1 min-h-screen bg-[url('/assets/child-shop-page-background.png')]  bg-fixed bg-center flex flex-col items-center px-4 py-3 h-200 overflow-y-auto"
-            >
-                <KidItem />
-                <KidItem />
-                <KidItem />
-                <KidItem />
-                <KidItem />
-            </div>
-        </>
+        <div className="min-h-screen bg-[url('/assets/child-shop-page-background.png')] bg-cover bg-center pt-24">
+            <ShopItemContainer />
+        </div>
     );
 };
 

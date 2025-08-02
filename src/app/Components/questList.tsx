c152f3b8fd73cdaca72838ba6616f2b94c672d64
ChikenNuget1 <samuel.choi111@gmail.com>
2025-08-02 17:21:21 +1200

    added comments

--- src/app/Components/questList.tsx
+++ src/app/Components/questList.tsx
@@ -19,16 +19,20 @@ const QuestList: React.FC<QuestListProps> = ({ quests, toggleQuest }) => {
         <div key={index} 
         style={{ fontFamily: 'var(--font-sour-gummy)' }}
         className="rounded-[60px] bg-white shadow-[6px_10px_0px_rgba(0,0,0,0.2)] p-8 flex flex-col items-center">
-          <p className="font-bold text-2xl mb-5 text-center">{quest.title}</p>
+          <p 
+          // This is the title
+          className="font-bold text-2xl mb-5 text-center">{quest.title}</p>
             <button
+            // This is the actual button inside each quest
             className={`text-center px-20 py-3 rounded-full 
                 shadow-[4px_3px_0px_rgba(0,0,0,0.2)] 
                 active:shadow-[0px_0px_0px_rgba(0,0,0,0.2)] 
                 active:translate-x-[3px] active:translate-y-[3px] 
                 transition-all duration-200 
                 text-white font--font-sour-gummy text-2xl 
-                ${quest.completed ? 'bg-green-600' : 'bg-red-600'}`}
+                ${quest.completed ? 'bg-green-900' : 'bg-red-600'}`}
             onClick={() => toggleQuest(index)}
+            // Probably a better way to implement th eonClick effect.
             >
             {quest.completed ? 'done' : 'not done'}
           </button>

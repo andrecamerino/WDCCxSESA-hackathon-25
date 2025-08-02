3ed9463ac5bbb988a446ed07eb1820c4352e47dd
Andre Camerino <camerinoandre@gmail.com>
2025-08-02 21:12:00 +1200

    add more example quests

--- src/app/data/QuestList.ts
+++ src/app/data/QuestList.ts
@@ -15,4 +15,13 @@ export const quests: Quest[] = [
   createQuest('Feed the pet'),
   createQuest('Clean your room'),
   createQuest('Brush your teeth'),
+  createQuest('Do your homework'),
+  createQuest('Help with dinner'),
+  createQuest('Water the plants'),
+  createQuest('Take out the trash'),
+  createQuest('Read for 20 minutes'),
+  createQuest('Organise your backpack'),
+  createQuest('Set the table'),
+  createQuest('Tidy your desk'),
+  createQuest('Make your bed'),
 ];

861fda1a106d6a23b833ed35cbcdc0f6f782e5a9
Andre Camerino <camerinoandre@gmail.com>
2025-08-03 06:28:40 +1200

    change to icon buttons

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,23 +1,38 @@
 import React from "react";
-import { GoHome } from "react-icons/go";
+import { FaHouse } from "react-icons/fa6";
+import { FaScroll } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+
 
 const Navbar = () => {
   return (
-    <div className="fixed bottom-0 w-full bg-white border-t border-gray-200 flex justify-around items-center py-3 z-999 h-[100px] text-2xl">
-      <a href="/QuestPage" className="text-gray-700 text-center">
-        Quest
+    <div className="fixed bottom-0 w-full bg-white border-t-6 border-l-6 border-r-6 border-black rounded-t-3xl flex justify-around items-center py-3 z-999 h-[100px] text-2xl ">
+      <a
+        href="/QuestPage"
+        style={{
+          borderRadius: "30% 70% 35% 65% / 58% 25% 75% 42% ",
+        }}
+        className="text-gray-700 bg-yellow-300 text-center flex items-center justify-center w-[70px] h-[70px] border-6 border-black"
+      >
+        <FaScroll className="text-black text-4xl font-bold" />
       </a>
       <a
         href="/Home"
         style={{
+          borderRadius: "54% 46% 56% 44% / 42% 58% 42% 58% ",
+        }}
+        className="text-gray-700 bg-[#F164A9] text-center flex items-center justify-center w-[70px] h-[70px] border-6 border-black"
+        >
+        <FaHouse className="text-black text-4xl font-bold" />
+      </a>
+      <a
+        href="/KidsShop"
+        style={{
           borderRadius: "68% 32% 63% 37% / 28% 72% 28% 72%",
         }}
-        className="text-gray-700 bg-[#F164A9] text-center flex items-center justify-center w-[70px] h-[70px]"
+        className="text-gray-700 bg-green-300 text-center flex items-center justify-center w-[70px] h-[70px] border-6 border-black"
       >
-        <GoHome className="text-white text-4xl" />
-      </a>
-      <a href="/KidsShop" className="text-gray-700 text-center">
-        Shop
+        <FaShoppingCart className="text-black text-4xl font-bold" />
       </a>
     </div>
   );

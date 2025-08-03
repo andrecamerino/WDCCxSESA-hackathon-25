e9d778dd30b8ba0592d7aff7f60f3859a8fd0e7f
Andre Camerino <camerinoandre@gmail.com>
2025-08-03 12:09:57 +1200

    update child schema

--- models/Child.js
+++ models/Child.js
@@ -3,14 +3,23 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const childSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    money: {
-        type: Number,
-        default: 0,
-    }
-}, {timestamps: true});
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  money: {
+    type: Number,
+    required: true,
+  },
+}, { timestamps: true });
 
 export default mongoose.models.Child || mongoose.model("Child", childSchema);
